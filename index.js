a873ab04833ded63e21b9026ca3e63fe449921a7
Caleb Griffin <cgriffin.7622@gmail.com>
2019-02-17 22:48:45 -0500

    Random Number logic added

---
+++ index.js
@@ -0,0 +1,27 @@
+// var randomNumber1 = Math.random();
+// randomNumber1 = randomNumber1 * 6;
+// randomNumber1 = Math.floor(randomNumber1) + 1;
+
+
+function randomNumberGenerator(topNumber) {
+  var randomNumber = Math.random();
+  randomNumber = randomNumber * topNumber;
+  randomNumber = Math.floor(randomNumber) + 1;
+
+  return randomNumber;
+}
+
+var randomNumber1 = randomNumberGenerator(6);
+var randomNumber2 = randomNumberGenerator(6);
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins! 🚩";
+} else if (randomNumber1 === randomNumber2) {
+  document.querySelector("h1").innerHTML = "It's a Tie!";
+} else {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
